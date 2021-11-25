a2c625a58214707281aefbfbecd679bc9d725860
Halk Liff <halkliff@pm.me>
2021-11-25 20:27:13 -0300

    fix: eslint was not working, now it should work flawlessly

--- scripts/_utils/log.ts
+++ scripts/_utils/log.ts
@@ -1,24 +1,25 @@
+/* eslint-disable no-console */
 import chalk from 'chalk';
 import dayjs from 'dayjs';
 
 export const logDate = (date = new Date()) =>
   chalk`{cyan [ ${dayjs(date).format('YYYY-MM-DD HH:mm')} ]}`;
 
-export const error = (error: Error, message?: string) =>
+export const error = (err: Error, message?: string) =>
   console.error(
     `${logDate(new Date())} ${chalk.red.bold('[ ERROR ]:')} ${chalk.redBright(
-      message ?? error.message,
-    )}${error.stack ? '\n' + chalk.redBright(error.stack) : ''}`,
+      message ?? err.message,
+    )}${err.stack ? `\n${chalk.redBright(err.stack)}` : ''}`,
   );
 
-export const info = (info: string) =>
+export const info = (message: string) =>
   console.info(
-    `${logDate(new Date())} ${chalk.grey.bold('[ INFO ]:')} ${info}`,
+    `${logDate(new Date())} ${chalk.grey.bold('[ INFO ]:')} ${message}`,
   );
 
-export const warning = (warning: string) =>
+export const warning = (message: string) =>
   console.warn(
     `${logDate(new Date())} ${chalk.yellow.bold(
       '[ WARNING ]:',
-    )} ${chalk.yellowBright(warning)}`,
+    )} ${chalk.yellowBright(message)}`,
   );
